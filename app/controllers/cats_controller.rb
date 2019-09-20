71ec1ea251af49f7de2c1a08671bef95b6732832
adamjacobcruse@gmail.com <learn@learnT7.local>
2019-09-19 17:20:24 -0700

    added show 1 cat and update method

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -5,12 +5,27 @@ class CatsController < ApplicationController
         render json: cats
     end
 
+    def show
+        cats=Cat.find(params[:id])
+        render json: cat
+    end
+
     def create
         cat = Cat.create(cat_params)
         if cat.valid?
             render json: cat
             else
               render json: cat.errors, status: :unprocessable_entity
+          end
+      end
+
+      def update
+          cat= Cat.find(params[:id])
+          if cat.update(cat_params)
+              render json: cat
+          else
+              render json: cat.errors,
+              status: :unprocessable_entity
           end
       end
 
